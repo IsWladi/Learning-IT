eb96be86e59a03ace116e5955d82cd5ddff8dd9a
IsWladi <wladimir2000@protonmail.com>
2025-07-20 17:22:31 -0400

    chore: add site info

--- web_astro/astro.config.mjs
+++ web_astro/astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://learnit.iswlcloud.com",
   integrations: [
     starlight({
       title: "Home",
